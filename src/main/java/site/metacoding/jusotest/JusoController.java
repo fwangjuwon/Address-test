650ba85936fa5bdaddd085af1ca049a95ace87bd
LeeSeonHo113 <fwangjuwon@naver.com>
2022-05-23 14:45:17 +0900

    스토어 사용방식 -방법2

--- src/main/java/site/metacoding/jusotest/JusoController.java
+++ src/main/java/site/metacoding/jusotest/JusoController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JusoController {
@@ -16,9 +17,15 @@ public class JusoController {
         return "home";
     }
 
+    // 해당 콜백이 호출되면 브라우저한테 푸쉬해주는게 좋다. (stream이 유지되고 있어야한다. - 방법 2가지: 웹소켓, sse)
     @PostMapping("/juso/callback")
     public void jusoCallback(String roadFullAddr) {
         System.out.println(roadFullAddr);
+        Store.roadFullAddr = roadFullAddr;
     }
 
+    @GetMapping("/juso/check")
+    public @ResponseBody String jusoCheck() {
+        return Store.roadFullAddr;
+    }
 }
---
+++ src/main/java/site/metacoding/jusotest/Store.java
@@ -0,0 +1,8 @@
+package site.metacoding.jusotest;
+
+//Redis (세션 저장소) 를 사용해야한다. 혹은 세션
+public class Store {
+
+    public static String roadFullAddr;
+
+}
